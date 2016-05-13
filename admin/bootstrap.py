d197ede0611ae90fe1107d2c0da5da7ad680eabb
Mark Granroth-Wilding <marktwilding@gmail.com>
2016-05-13 15:40:52 +0100

    Completed basic bootstrapping script for fetching Pimlico for a given config file.

--- admin/bootstrap.py
+++ admin/bootstrap.py
@@ -11,6 +11,7 @@ directory, and call `python bootstrap.py`.
 """
 import sys
 import os
+import tarfile
 import urllib2
 
 
@@ -19,7 +20,7 @@ RAW_URL = "%s/raw/master/" % REPOSITORY_URL
 
 
 def lookup_pimlico_versions():
-    release_list_url = "%sadmin/releases.txt"
+    release_list_url = "%sadmin/releases.txt" % RAW_URL
     try:
         release_data = urllib2.urlopen(release_list_url).read()
     except Exception, e:
@@ -65,9 +66,29 @@ def find_config_value(config_path, key, start_in_pipeline=False):
     return
 
 
+def extract(tar_path):
+    extract_path = os.path.dirname(tar_path)
+    with tarfile.open(tar_path, "r:gz") as tar:
+        for item in tar:
+            tar.extract(item, extract_path)
+
+
+def tar_dirname(tar_path):
+    with tarfile.open(tar_path, "r:gz") as tar:
+        # Expect first member to be a directory
+        member = tar.next()
+        if not member.isdir():
+            raise ValueError("downloaded tar file was expected to contain a directory, but didn't")
+        return member.name
+
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     current_dir = os.path.abspath(os.path.dirname(__file__))
+
+    if os.path.exists(os.path.join(current_dir, "pimlico")):
+        print "Pimlico source directory already exists: delete it if you want to fetch again"
+        sys.exit(1)
 
     if len(args) == 0:
         # Starting a new project in the current directory
@@ -76,7 +97,34 @@ if __name__ == "__main__":
         config_file = os.path.abspath(args[0])
         # Check the config file to find the version of Pimlico we need
         version = find_config_value(config_file, "release")
+        major_version = int(version.partition(".")[0])
         print "Config file requires Pimlico version %s" % version
-        bleeding_edge = lookup_bleeding_edge()
-        print "Bleeding edge release: %s" % bleeding_edge
-        # TODO Get Pimlico
+
+        available_releases = lookup_pimlico_versions()
+        bleeding_edge = available_releases[-1]
+        tags = available_releases[:-1]
+
+        # If the bleeding edge version is compatible (same major version) just use that
+        if int(bleeding_edge.lstrip("v").partition(".")[0]) == major_version:
+            print "Bleeding edge (%s) is compatible" % bleeding_edge
+            fetch_release = "master"
+        else:
+            # Find the latest release that has the same major version
+            compatible_tags = [t for t in tags if int(t.lstrip("v").partition(".")[0]) == major_version]
+            fetch_release = compatible_tags[-1]
+            print "Fetching latest release of major version %s, which is %s" % (major_version, fetch_release)
+
+        archive_url = "%srepository/archive.tar.gz?ref=%s" % (REPOSITORY_URL, fetch_release)
+        print "Downloading Pimlico source code from %s" % archive_url
+        tar_download_path = os.path.join(current_dir, "archive.tar.gz")
+        with open(tar_download_path, "wb") as archive_file:
+            archive_file.write(urllib2.urlopen(archive_url).read())
+
+        print "Extracting source code"
+        extracted_dirname = tar_dirname(tar_download_path)
+        extract(tar_download_path)
+        # Extracted source code: remove the archive
+        os.remove(tar_download_path)
+
+        os.rename(os.path.join(current_dir, extracted_dirname), os.path.join(current_dir, "pimlico"))
+        print "Pimlico source (%s) is now available in directory pimlico/" % fetch_release
