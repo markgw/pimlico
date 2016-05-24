98ab19043de8b7a744bdf651c70b3a5bc2bc2da3
Mark Granroth-Wilding <marktwilding@gmail.com>
2016-05-24 13:56:35 +0100

    Fixed path for RTD build

--- setup.py
+++ setup.py
@@ -4,37 +4,12 @@ This is provided primarily so that the docs can be built on the ReadTheDocs serv
 code, including basic dependencies.
 
 """
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath('src/python'))
+
 # Install the basic, core Pimlico dependencies
 from pimlico import install_core_dependencies
 install_core_dependencies()
-
-"""
-import os
-from distutils.command.install import install as _install
-from setuptools import find_packages, setup
-
-
-def _post_install(dir):
-    from subprocess import call
-    call(["make"], cwd=os.path.join(dir, "lib", "python"))
-
-
-class install(_install):
-    def run(self):
-        _install.run(self)
-        self.execute(_post_install, (self.install_lib,), msg="Running post install task to install basic libs")
-
-
-setup(
-    name="Pimlico",
-    version="0.1",
-    author="Mark Granroth-Wilding",
-    package_dir={"pimlico": "src/python/pimlico"},
-    packages=find_packages("./src/python"),
-    package_data={
-        "": ["lib/*", "bin/*", "models/*", "examples/*", "build/*"]
-    },
-    include_package_data=True,
-    #cmdclass={'install': install},
-)
-"""
