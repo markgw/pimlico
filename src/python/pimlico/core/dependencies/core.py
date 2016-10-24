58b63b49ad5612ff77aef24a651eca13ee844889
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2016-10-24 16:01:30 +0300

    A small further doc tweak

--- src/python/pimlico/core/dependencies/core.py
+++ src/python/pimlico/core/dependencies/core.py
@@ -1,10 +1,9 @@
-
-"""
-Core dependencies required by the basic Pimlico installation, regardless of what pipeline is being processed.
-
-"""
+""" Basic Pimlico core dependencies """
 from pimlico.core.dependencies.python import PythonPackageSystemwideInstall, PythonPackageOnPip
 
+#: Core dependencies required by the basic Pimlico installation, regardless of what pipeline is being processed.
+#: These will be checked when Pimlico is run, using the same dependency-checking mechanism that Pimlico modules
+#: use, and installed automatically if they're not found.
 CORE_PIMLICO_DEPENDENCIES = [
     # Virtualenv must be installed so that we can install other packages in the local Pimlico environment
     PythonPackageSystemwideInstall("virtualenv", "Virtualenv"),
