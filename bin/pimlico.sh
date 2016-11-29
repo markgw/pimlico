7962c7ec15f527fbb3f9061b92837ad19d0d74ee
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2016-11-29 12:07:29 +0200

    Fixed Mac compatibility problem again
    Fixed problem with getting absolute location of scripts from Bash on Mac.
    
    Documented pimlico.sh and the python wrapper better (in comments).

--- bin/pimlico.sh
+++ bin/pimlico.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the directory containing this script, following any symlinks
+# Usual Bash solutions don't work across Max and Linux, so this Perl alternative is better
+THIS=$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")
+DIR=$(dirname $THIS)
+
+# In the same directory as this script, in the Pimlico codebase, lives a Python wrapper script
+# This ensures the virtualenv is set up, prepares it if not, then calls Python through the virtualenv
 $DIR/python -m pimlico.cli.run $*
