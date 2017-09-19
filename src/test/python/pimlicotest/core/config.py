b041c163eaace3fdb701fd044e17e2e6975409d7
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2017-09-19 13:50:38 +0300

    Updated testing
    Pimlico currently has barely any tests. Updated what's there, so we're now ready to start adding unit tests for the core codebase.
    
    Small contribution to #7.

--- src/test/python/pimlicotest/core/config.py
+++ src/test/python/pimlicotest/core/config.py
@@ -1,13 +1,48 @@
+import tempfile
 import unittest
 from pimlicotest import example_path
-from pimlico.core.config import PipelineConfig
 
 
-class TestEmptyPipelineLoading(unittest.TestCase):
+class PipelineConfigTest(unittest.TestCase):
+    def setUp(self):
+        # Get a basic local config file so Pimlico doesn't go looking for one on the system
+        self.local_conf_path = example_path("tests_local_config")
+        # Create a temporary directory to use as our storage location
+        self.storage_dir = tempfile.mkdtemp()
+        # Override the local config values that should point to this path
+        self.override_local_config = {
+            "long_term_store": self.storage_dir,
+            "short_term_store": self.storage_dir,
+        }
+
+    def tearDown(self):
+        import shutil
+        shutil.rmtree(self.storage_dir)
+
+
+class TestEmptyPipelineLoading(PipelineConfigTest):
+    """
+    Load a pipeline config file that doesn't contain any modules
+    """
     def test_load(self):
+        from pimlico.core.config import PipelineConfig
+
         # Load a config file
         conf_path = example_path("empty.conf")
-        pipeline = PipelineConfig.load(conf_path)
+        pipeline = PipelineConfig.load(conf_path,
+                                       local_config=self.local_conf_path,
+                                       override_local_config=self.override_local_config)
+
+
+class TestEmptyPipelineCreation(PipelineConfigTest):
+    """
+    Create an empty pipeline programmatically.
+
+    """
+    def test_load(self):
+        from pimlico.core.config import PipelineConfig
+        pipeline = PipelineConfig.empty(local_config=self.local_conf_path,
+                                        override_local_config=self.override_local_config)
 
 
 if __name__ == "__main__":
