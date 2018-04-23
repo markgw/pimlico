dc5e99b3491276ef28e1d65a90446d4aca118825
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-04-23 19:13:04 +0300

    Added formatters for tokenized datatypes

--- src/python/pimlico/datatypes/formatters/tokenized.py
+++ src/python/pimlico/datatypes/formatters/tokenized.py
@@ -1,5 +1,6 @@
 from pimlico.cli.browser.formatter import DocumentBrowserFormatter
-from pimlico.datatypes.tokenized import TokenizedDocumentType
+from pimlico.datatypes.tokenized import TokenizedDocumentType, CharacterTokenizedDocumentType, \
+    SegmentedLinesDocumentType
 
 
 class TokenizedDocumentFormatter(DocumentBrowserFormatter):
@@ -16,3 +17,26 @@ class TokenizedDocumentFormatter(DocumentBrowserFormatter):
         else:
             doc = u"\n".join(u" ".join(words) for words in doc)
         return doc
+
+
+class CharacterTokenizedDocumentFormatter(DocumentBrowserFormatter):
+    DATATYPE = CharacterTokenizedDocumentType
+
+    def __init__(self, corpus, raw_data=False):
+        super(CharacterTokenizedDocumentFormatter, self).__init__(corpus)
+        self.raw_data = raw_data
+
+    def format_document(self, doc):
+        if self.raw_data:
+            # We're just showing the raw data, so don't try to do anything other than ensure it's a string
+            doc = unicode(doc)
+        else:
+            doc = u"\n".join(u"".join(chars) for chars in doc)
+        return doc
+
+
+class SegmentedLinesFormatter(DocumentBrowserFormatter):
+    DATATYPE = SegmentedLinesDocumentType
+
+    def format_document(self, doc):
+        return u"\n".join(" ".join(line) for line in doc)
