8fb018ca8ac9012a2f8a21f540adee2dfb8f8620
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-04-24 18:24:24 +0300

    Small improvement to simple tokenize module

--- src/python/pimlico/modules/text/simple_tokenize/execute.py
+++ src/python/pimlico/modules/text/simple_tokenize/execute.py
@@ -5,8 +5,9 @@ def process_document(worker, archive_name, doc_name, doc):
     # We receive plain text input
     # Just split up lines and split each line on spaces (or the given splitter)
     # Also strip whitespace from lines
+    # If there are multiple splitters in a row (resulting in empty tokens) ignore them
     return [
-        line.strip().split(worker.info.options["splitter"])
+        [t for t in line.strip().split(worker.info.options["splitter"]) if len(t)]
         for line in doc.splitlines()
     ]
 
