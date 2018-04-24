930f8730a72733905e6ba4c038e6acea191fe803
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-04-24 18:24:46 +0300

    Added module to perform frequency count for vocab
    Vocab does not store frequency counts for its items. This module reads a corpus and counts the tokens, putting them in an array, which can then be used by other modules that need frequency counts.

---
+++ src/python/pimlico/modules/corpora/vocab_counter/execute.py
@@ -0,0 +1,37 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+from collections import Counter
+
+import numpy
+
+from pimlico.core.modules.base import BaseModuleExecutor
+from pimlico.datatypes.arrays import NumpyArrayWriter
+from pimlico.utils.progress import get_progress_bar
+
+
+class ModuleExecutor(BaseModuleExecutor):
+    def execute(self):
+        corpus = self.info.get_input("corpus")
+        vocab = self.info.get_input("vocab")
+        dist_len = len(vocab)+1 if self.info.options["oov_excluded"] else len(vocab)
+
+        self.log.info("Counting token frequencies in corpus")
+        pbar = get_progress_bar(len(corpus))
+        # Iterate over the whole corpus, counting up tokens
+        counts = Counter(
+            token for doc_name, doc in pbar(corpus) for line in doc for token in line
+        )
+        self.log.info("Counts collected")
+        # Put the result in a numpy array
+        dist = numpy.array([counts.get(i, 0) for i in range(dist_len)])
+
+        # Output most and least frequent tokens
+        ordered_ids = numpy.argsort(dist)[0]
+        _id2token = lambda i: "OOV" if i >= len(vocab) else vocab.id2token[i]
+        _fmt_ids = lambda ids: u", ".join(u"{} ({})".format(_id2token(i), dist[i]) for i in ids)
+        self.log.info(u"{}, ..., {}".format(_fmt_ids(ordered_ids[:5]), _fmt_ids(ordered_ids[-5:])))
+
+        # Store the output array
+        with NumpyArrayWriter(self.info.get_absolute_output_dir("distribution")) as writer:
+            writer.set_array(dist)
---
+++ src/python/pimlico/modules/corpora/vocab_counter/info.py
@@ -0,0 +1,45 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+"""
+Count the frequency of each token of a vocabulary in a given corpus (most often
+the corpus on which the vocabulary was built).
+
+Note that this distribution is not otherwise available along with the vocabulary.
+It stores the document frequency counts - how many documents each token appears
+in - which may sometimes be a close enough approximation to the actual frequencies.
+But, for example, when working with character-level tokens, this estimate will
+be very poor.
+
+The output will be a 1D array whose size is the length of the vocabulary, or
+the length plus one, if oov_excluded=T (used if the corpus has been mapped
+so that OOVs are represented by the ID vocab_size+1, instead of having a
+special token).
+
+"""
+from pimlico.datatypes import NumpyArray
+
+from pimlico.core.dependencies.python import numpy_dependency
+from pimlico.core.modules.base import BaseModuleInfo
+from pimlico.core.modules.options import str_to_bool
+from pimlico.datatypes.dictionary import Dictionary
+from pimlico.datatypes.ints import IntegerListsDocumentType
+from pimlico.datatypes.tar import TarredCorpusType
+
+
+class ModuleInfo(BaseModuleInfo):
+    module_type_name = "vocab_counter"
+    module_readable_name = "Token frequency counter"
+    module_inputs = [("text", TarredCorpusType(IntegerListsDocumentType)), ("vocab", Dictionary)]
+    module_outputs = [("distribution", NumpyArray)]
+    module_options = {
+        "oov_excluded": {
+            "help": "Indicates that the corpus has been mapped so that OOVs are represented "
+                    "by the ID vocab_size+1, instead of having a special token in the vocab",
+            "type": str_to_bool,
+        },
+    }
+
+    def get_software_dependencies(self):
+        return [numpy_dependency] + super(ModuleInfo, self).get_software_dependencies()
+
