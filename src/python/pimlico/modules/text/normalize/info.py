4f730a7de8ba6d5f2c8035f5b129805433146112
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-05-12 10:53:07 +0300

    Added normalize module
    Simple normalization. Can be extended later to do more.

---
+++ src/python/pimlico/modules/text/normalize/info.py
@@ -0,0 +1,36 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+"""
+Perform text normalization on tokenized documents.
+
+Currently, this includes only case normalization (to upper or lower case). In
+the future, more normalization operations may be added.
+
+"""
+from pimlico.core.modules.map import DocumentMapModuleInfo
+
+from pimlico.core.modules.options import choose_from_list
+from pimlico.datatypes.tar import TarredCorpusType
+from pimlico.datatypes.tar import TarredCorpusWriter
+from pimlico.datatypes.tokenized import TokenizedCorpus
+from pimlico.datatypes.tokenized import TokenizedDocumentType
+
+
+class ModuleInfo(DocumentMapModuleInfo):
+    module_type_name = "normalize"
+    module_readable_name = "Normalize tokenized text"
+    module_inputs = [("corpus", TarredCorpusType(TokenizedDocumentType))]
+    module_outputs = [("corpus", TokenizedCorpus)]
+    module_options = {
+        "case": {
+            "help": "Transform all text to upper or lower case. Choose from 'upper' or 'lower', "
+                    "or leave blank to not perform transformation",
+            "default": u"",
+            "type": choose_from_list(["upper", "lower", ""]),
+        },
+    }
+
+    def get_writer(self, output_name, output_dir, append=False):
+        if output_name == "corpus":
+            return TarredCorpusWriter(output_dir, append=append)
