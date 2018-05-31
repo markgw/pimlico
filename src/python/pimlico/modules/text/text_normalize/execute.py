0d5f8efed8d012f21e3cc667a2a6ffeaa85fc8cb
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-05-31 09:56:59 +0300

    Added raw text normalization module
    Same as normalize, but works on raw text instead of tokenized text. This gives a slightly different set of things you might want to do (e.g. remove blank lines).

---
+++ src/python/pimlico/modules/text/text_normalize/execute.py
@@ -0,0 +1,31 @@
+from pimlico.core.modules.map import skip_invalid
+
+from pimlico.core.modules.map.multiproc import multiprocessing_executor_factory
+
+
+def worker_setup(worker):
+    worker.case = worker.executor.info.options["case"]
+    worker.strip = worker.executor.info.options["strip"]
+    worker.blank_lines = worker.executor.info.options["blank_lines"]
+
+
+@skip_invalid
+def process_document(worker, archive_name, doc_name, doc):
+    # First split into lines, since much works on the line level
+    lines = doc.splitlines()
+
+    if worker.case == "upper":
+        lines = [line.upper() for line in lines]
+    elif worker.case == "lower":
+        lines = [line.lower() for line in lines]
+
+    if worker.strip:
+        lines = [line.strip() for line in lines]
+
+    if worker.blank_lines:
+        lines = [l for l in lines if len(l) > 0]
+
+    return u"\n".join(lines)
+
+
+ModuleExecutor = multiprocessing_executor_factory(process_document, worker_set_up_fn=worker_setup)
---
+++ src/python/pimlico/modules/text/text_normalize/info.py
@@ -0,0 +1,40 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+"""
+Text normalization for raw text documents.
+
+"""
+from pimlico.core.modules.map import DocumentMapModuleInfo
+
+from pimlico.core.modules.options import choose_from_list, str_to_bool
+from pimlico.datatypes.documents import TextDocumentType
+from pimlico.datatypes.tar import TarredCorpusType, RawTextTarredCorpus
+from pimlico.datatypes.tar import TarredCorpusWriter
+
+
+class ModuleInfo(DocumentMapModuleInfo):
+    module_type_name = "text_normalize"
+    module_readable_name = "Normalize raw text"
+    module_inputs = [("corpus", TarredCorpusType(TextDocumentType))]
+    module_outputs = [("corpus", RawTextTarredCorpus)]
+    module_options = {
+        "case": {
+            "help": "Transform all text to upper or lower case. Choose from 'upper' or 'lower', "
+                    "or leave blank to not perform transformation",
+            "default": u"",
+            "type": choose_from_list(["upper", "lower", ""]),
+        },
+        "strip": {
+            "help": "Strip whitespace from the start and end of lines",
+            "type": str_to_bool,
+        },
+        "blank_lines": {
+            "help": "Remove all blank lines (after whitespace stripping, if requested)",
+            "type": str_to_bool,
+        },
+    }
+
+    def get_writer(self, output_name, output_dir, append=False):
+        if output_name == "corpus":
+            return TarredCorpusWriter(output_dir, append=append)
