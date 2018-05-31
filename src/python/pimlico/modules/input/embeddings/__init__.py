3f60452cda8b6a60b91e8960eb0e04fb9f51043c
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-05-31 10:41:46 +0300

    Filled holes in docs
    Python modules containing Pimlico modules that didn't have names or descriptions.

---
+++ src/python/pimlico/modules/input/embeddings/__init__.py
@@ -0,0 +1,8 @@
+"""Embeddings
+
+Read vector embeddings (e.g. word embeddings) from various storage formats.
+
+There are several formats in common usage. Here we currently wrap only a couple, but there are
+others (e.g. word2vec binary) that would be worth including as well.
+
+"""
---
+++ src/python/pimlico/modules/input/text_annotations/__init__.py
@@ -0,0 +1,11 @@
+"""Annotated text
+
+Datasets that store text with accompanying annotations, like POS tags or dependency parses.
+
+There are lots of different ways of storing this type of data in common usage. Here we currently
+only implement variants on one -- the VRT format, used by Korp. In future, others should be
+added, e.g. CoNLL dependency parses.
+
+Datatypes exist for some of these, which should be converted to input readers in due course.
+
+"""
