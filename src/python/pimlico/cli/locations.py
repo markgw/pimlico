decce8e331ed1367622b282af06cf12cfaeffa61
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-08-28 12:20:25 +0300

    Fixed inputs cmd
    Wasn't fully updated for new datatypes

--- src/python/pimlico/cli/locations.py
+++ src/python/pimlico/cli/locations.py
@@ -33,32 +33,36 @@ class InputsCmd(PimlicoCLISubcommand):
         # Display info for each input to this module
         for input_name in module.input_names:
             print "\nInput '%s'" % input_name
-            input_lst = module.get_input(input_name, always_list=True)
+            input_setup_lst = module.get_input_reader_setup(input_name, always_list=True)
             input_connections = module.get_input_module_connection(input_name, always_list=True)
-            multiple_inputs = len(input_lst) > 1
+            multiple_inputs = len(input_setup_lst) > 1
             if multiple_inputs:
                 print "Multiple input sources: showing locations for all"
 
-            for i, (input_datatype, (prev_module, prev_output_name, __)) in \
-                    enumerate(zip(input_lst, input_connections)):
+            for i, (input_setup, (prev_module, prev_output_name)) in enumerate(zip(input_setup_lst, input_connections)):
                 if multiple_inputs:
                     print "## Input source %d ##" % i
 
-                if input_datatype.data_ready():
-                    corpus_dir = input_datatype.absolute_base_dir
-                    if corpus_dir is None:
-                        print "Data (%s) available, but no directory given: probably a filter datatype" % \
-                              input_datatype.full_datatype_name()
-                    else:
+                input_datatype = input_setup.datatype
+                if input_setup.ready_to_read():
+                    # Get the input reader
+                    input_reader = input_setup(pipeline, module_name)
+                    if hasattr(input_reader, "base_dir") and input_reader.base_dir is not None:
                         print "Data (%s) available in:" % input_datatype.full_datatype_name()
-                        print " - %s" % corpus_dir
+                        print " - %s" % input_reader.base_dir
+                    else:
+                        print "Data (%s) available, but no directory given: perhaps a filter datatype" % \
+                              input_datatype.full_datatype_name()
                 else:
-                    print "Data not available. Data will be found in any of the following locations:"
-                    # Get the relative dir within the Pimlico dir structures
-                    rel_path = prev_module.get_output_dir(prev_output_name)
-                    # Resolve this to all possible absolute dirs (usually two)
-                    abs_paths = remove_duplicates([path for (name, path) in pipeline.get_data_search_paths(rel_path)])
-                    print "\n".join(" - %s" % pth for pth in abs_paths)
+                    if hasattr(input_setup, "data_paths") and len(input_setup.data_paths) > 0:
+                        print "Data not available. Data will be found in any of the following locations:"
+                        # Get the relative dir within the Pimlico dir structures
+                        rel_path = prev_module.get_output_dir(prev_output_name)
+                        # Resolve this to all possible absolute dirs (usually two)
+                        abs_paths = remove_duplicates([path for (name, path) in pipeline.get_data_search_paths(rel_path)])
+                        print "\n".join(" - %s" % pth for pth in abs_paths)
+                    else:
+                        print "Data not available. No possible data locations available, perhaps a filter datatype"
 
 
 class OutputCmd(PimlicoCLISubcommand):
