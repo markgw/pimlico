94a217e93e5fa51d7930ce3b712042ec15022831
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-09-04 15:05:19 +0300

    Finished implementing sklearn log reg module

---
+++ src/python/pimlico/modules/sklearn/logistic_regression/execute.py
@@ -0,0 +1,48 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+
+from pimlico.core.config import PipelineConfigParseError
+from pimlico.core.modules.base import BaseModuleExecutor
+from pimlico.old_datatypes.arrays import NumpyArrayWriter
+from pimlico.modules.sklearn.matrix_factorization.info import SKLEARN_CLASSES
+
+
+class ModuleExecutor(BaseModuleExecutor):
+    def execute(self):
+        input_matrix = self.info.get_input("matrix").array
+        self.log.info("Loaded input matrix: %s" % str(input_matrix.shape))
+
+        transform_type = self.info.options["class"]
+        # Check known properties of this type of transformation
+        transform_props = SKLEARN_CLASSES[transform_type]
+        # Check whether this type is able to accept sparse input
+        if transform_props["sparse"]:
+            # Convert to CSR, which is better for most purposes
+            input_matrix = input_matrix.tocsr()
+        else:
+            # We need to transform the matrix to a dense array
+            # This could cause problems with large matrices, in which case you need to use a different transform type
+            self.log.info("%s transformation requires dense input: converting matrix" % transform_type)
+            input_matrix = input_matrix.toarray()
+
+        # Try initializing the transformation
+        self.log.info(
+            "Initializing %s with options: %s" %
+            (transform_type, ", ".join("%s=%s" % (key, val) for (key, val) in self.info.init_kwargs.items()))
+        )
+        transform_cls = self.info.load_transformer_class()
+        try:
+            transformer = transform_cls(**self.info.init_kwargs)
+        except TypeError, e:
+            raise PipelineConfigParseError("invalid arguments to %s: %s" % (transform_type, e))
+
+        # Apply transformation to the matrix
+        self.log.info("Fitting %s transformation on input matrix" % transform_type)
+        transformed_matrix = transformer.fit_transform(input_matrix)
+        self.log.info("Fitting complete: storing H and W matrices")
+
+        with NumpyArrayWriter(self.info.get_absolute_output_dir("w")) as w_writer:
+            w_writer.set_array(transformed_matrix)
+        with NumpyArrayWriter(self.info.get_absolute_output_dir("h")) as h_writer:
+            h_writer.set_array(transformer.components_)
---
+++ src/python/pimlico/modules/sklearn/logistic_regression/info.py
@@ -0,0 +1,102 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+
+"""
+Provides a simple interface to `Scikit-Learn's <http://scikit-learn.org/stable/>`_ various matrix factorization
+models.
+
+Since they provide a consistent training interface, you can simply choose the class name of the method you
+want to use and specify options relevant to that method in the ``options`` option. For available options,
+take a look at the table of parameters in the
+`Scikit-Learn documentation <http://scikit-learn.org/stable/modules/classes.html#module-sklearn.decomposition>`_
+for each class.
+
+.. todo::
+
+   Update to new datatypes system and add test pipeline
+
+"""
+import json
+
+from pimlico.core.config import PipelineConfigParseError
+from pimlico.core.dependencies.python import PythonPackageOnPip
+from pimlico.core.modules.base import BaseModuleInfo
+from pimlico.core.modules.options import choose_from_list
+from pimlico.old_datatypes.arrays import ScipySparseMatrix, NumpyArray
+
+
+SKLEARN_CLASSES = {
+    "PCA": {
+        "sparse": False,
+    },
+    "ProjectedGradientNMF": {
+        "sparse": True
+    },
+    "RandomizedPCA": {
+        "sparse": True,
+    },
+    "FactorAnalysis": {
+        "sparse": False,  # I think...?
+    },
+    "FastICA": {
+        "sparse": False,  # Don't know
+    },
+    "TruncatedSVD": {
+        "sparse": True,
+    },
+    "NMF": {
+        "sparse": True,
+    },
+    "SparsePCA": {
+        "sparse": True,  # That's the point...
+    },
+    "LatentDirichletAllocation": {
+        "sparse": True,
+    },
+}
+
+
+class ModuleInfo(BaseModuleInfo):
+    module_type_name = "sklearn_mat_fac"
+    module_readable_name = "Sklearn matrix factorization"
+    module_inputs = [("matrix", ScipySparseMatrix)]
+    module_outputs = [("w", NumpyArray), ("h", NumpyArray)]
+    module_options = {
+        "class": {
+            "help": "Scikit-learn class to use to fit the matrix factorization. Should be the name of a class in "
+                    "the package sklearn.decomposition that has a fit_transform() method and a components\\_ attribute. "
+                    "Supported classes: %s" % ", ".join(SKLEARN_CLASSES),
+            "type": choose_from_list(SKLEARN_CLASSES.keys()),
+            "required": True,
+        },
+        "options": {
+            "help": "Options to pass into the constructor of the sklearn class, formatted as a JSON dictionary "
+                    "(potentially without the {}s). E.g.: 'n_components=200, solver=\"cd\", tol=0.0001, max_iter=200'",
+        },
+    }
+
+    def __init__(self, module_name, pipeline, **kwargs):
+        super(ModuleInfo, self).__init__(module_name, pipeline, **kwargs)
+        # Process JSON options
+        json_options = self.options["options"].strip()
+        # Add {}s if they're not in the input, to make the input format potentially nicer looking
+        if not json_options[0] == "{" or not json_options[-1] == "}":
+            json_options = "{%s}" % json_options
+        try:
+            self.init_kwargs = json.loads(json_options)
+        except ValueError:
+            raise PipelineConfigParseError("could not parse JSON options for scikit-learn module: %s" % json_options)
+        # Try loading the given transformer class name to check it's a valid one
+        try:
+            self.load_transformer_class()
+        except ImportError, e:
+            raise PipelineConfigParseError("Could not load decomposition class %s. Check it's available in the version "
+                                           "of scikit-learn you have installed" % self.options["class"])
+
+    def load_transformer_class(self):
+        from sklearn import decomposition
+        return getattr(decomposition, self.options["class"])
+
+    def get_software_dependencies(self):
+        return super(ModuleInfo, self).get_software_dependencies() + [PythonPackageOnPip("sklearn", "Scikit-learn")]
