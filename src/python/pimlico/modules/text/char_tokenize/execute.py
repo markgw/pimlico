de3e935ff552407ff84d9f533f4cc4457fbc8c8a
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-09-06 12:43:58 +0300

    Char tokenize skips invalid docs

--- src/python/pimlico/modules/text/char_tokenize/execute.py
+++ src/python/pimlico/modules/text/char_tokenize/execute.py
@@ -1,6 +1,8 @@
+from pimlico.core.modules.map import skip_invalid
 from pimlico.core.modules.map.multiproc import multiprocessing_executor_factory
 
 
+@skip_invalid
 def process_document(worker, archive_name, doc_name, doc):
     # We receive plain text input
     # Just split up lines and split lines up into characters
