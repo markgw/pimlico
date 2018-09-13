8355e2994266226552f320e3843a9221b39133b3
Mark Granroth-Wilding <markg@granroth-wilding.co.uk>
2018-09-13 10:14:21 +0300

    Deal with spaces in w2v format
    W2v format doesn't support spaces in words, since the voab format is space separated. Replace spaces when writing out. Assumes we invert replacement when reading.
    
    Do so when reading in using builtin w2v reader.

--- src/python/pimlico/modules/embeddings/store_word2vec/execute.py
+++ src/python/pimlico/modules/embeddings/store_word2vec/execute.py
@@ -8,6 +8,18 @@ class ModuleExecutor(BaseModuleExecutor):
         # Convert to Gensim KeyedVectors
         keyed_vectors = embeddings.to_keyed_vectors()
 
+        spaces = sum(1 for w in keyed_vectors.index2word if u" " in w or u"\u00A0" in w)
+        if spaces > 0:
+            self.log.info("Some words ({}) include spaces: replacing with '<space>', so they don't mess up the "
+                          "w2v vocab".format(spaces))
+            # If there are spaces in any of the words, this is a problem for the w2v format, since the vocab
+            # is stored in a space-separated file!
+            keyed_vectors.vocab = dict(
+                (replace_spaces(word), v) for (word, v) in keyed_vectors.vocab.items()
+            )
+            # Apply the same mapping to index2word
+            keyed_vectors.index2word = [replace_spaces(word) for word in keyed_vectors.index2word]
+
         # Output to the file
         with Word2VecFilesWriter(self.info.get_absolute_output_dir("embeddings")) as writer:
             # Use Gensim's output method to write in word2vec format
@@ -21,3 +33,7 @@ class ModuleExecutor(BaseModuleExecutor):
             # Both files have been written
             writer.file_written(writer.filenames[0])
             writer.file_written(writer.filenames[1])
+
+
+def replace_spaces(word):
+    return word.replace(u" ", u"<space>").replace(u"\u00A0", u"<nbspace>")
--- src/python/pimlico/modules/input/embeddings/word2vec/execute.py
+++ src/python/pimlico/modules/input/embeddings/word2vec/execute.py
@@ -1,3 +1,5 @@
+import os
+
 from gensim.models import KeyedVectors
 
 from pimlico.core.modules.base import BaseModuleExecutor
@@ -8,11 +10,33 @@ class ModuleExecutor(BaseModuleExecutor):
         path = self.info.options["path"]
         binary = self.info.options["binary"]
 
+        # See whether a vocab file was provided: typically it has the same basename and a "vocab" extension
+        vocab_path = "{}.vocab".format(path.rpartition(".")[0])
+        if os.path.exists(vocab_path):
+            self.log.info("Reading vocab from {}".format(vocab_path))
+        else:
+            self.log.info("No vocab file found")
+            # No vocab file: don't pass one into the reader
+            vocab_path = None
+
         # Use Gensim's tool to load the data
         self.log.info("Loading word2vec vectors from {}".format(path))
-        vectors = KeyedVectors.load_word2vec_format(path, binary=binary)
+        vectors = KeyedVectors.load_word2vec_format(path, binary=binary, fvocab=vocab_path)
+
+        # If there were spaces in any of the words, this is a problem for the w2v format, since the vocab
+        # is stored in a space-separated file! When Pimlico writes out w2v vectors, it replaces spaces,
+        # so we reverse that now
+        vectors.vocab = dict(
+            (replace_spaces(word), v) for (word, v) in vectors.vocab.items()
+        )
+        # Also apply inverse mapping to index2word
+        vectors.index2word = [replace_spaces(word) for word in vectors.index2word]
 
         # Get the vectors from the model
         # We can use these to store embeddings in the Pimlico format
         with self.info.get_output_writer() as writer:
             writer.write_keyed_vectors(vectors)
+
+
+def replace_spaces(word):
+    return word.replace(u"<space>", u" ").replace(u"<nbspace>", u"\u00A0")
