60e55e7795d5e21f95128d1baf5021b8cd8183ae
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-01-09 15:49:29 +0200

    Updated vocab counter module to new datatypes
    Got the vocab counter module working and added a test pipeline for it. Added required prepare datasets for the new pipeline.
    
    Also added test pipeline for vocab_mapper, which was missing before.

--- src/python/pimlico/modules/corpora/vocab_counter/info.py
+++ src/python/pimlico/modules/corpora/vocab_counter/info.py
@@ -1,40 +1,36 @@
+# coding=utf-8
 # This file is part of Pimlico
 # Copyright (C) 2016 Mark Granroth-Wilding
 # Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
-"""
+u"""
 Count the frequency of each token of a vocabulary in a given corpus (most often
 the corpus on which the vocabulary was built).
 
 Note that this distribution is not otherwise available along with the vocabulary.
-It stores the document frequency counts - how many documents each token appears
-in - which may sometimes be a close enough approximation to the actual frequencies.
+It stores the document frequency counts – how many documents each token appears
+in – which may sometimes be a close enough approximation to the actual frequencies.
 But, for example, when working with character-level tokens, this estimate will
 be very poor.
 
 The output will be a 1D array whose size is the length of the vocabulary, or
-the length plus one, if oov_excluded=T (used if the corpus has been mapped
-so that OOVs are represented by the ID vocab_size+1, instead of having a
+the length plus one, if ``oov_excluded=T`` (used if the corpus has been mapped
+so that OOVs are represented by the ID ``vocab_size+1``, instead of having a
 special token).
 
-.. todo::
-
-   Update to new datatypes system and add test pipeline
-
 """
-from pimlico.old_datatypes.arrays import NumpyArray
 from pimlico.core.dependencies.python import numpy_dependency
 from pimlico.core.modules.base import BaseModuleInfo
 from pimlico.core.modules.options import str_to_bool
-from pimlico.old_datatypes.dictionary import Dictionary
-from pimlico.old_datatypes.ints import IntegerListsDocumentType
-from pimlico.old_datatypes.tar import TarredCorpusType
+from pimlico.datatypes import GroupedCorpus, Dictionary
+from pimlico.datatypes.arrays import NumpyArray
+from pimlico.datatypes.corpora.ints import IntegerListsDocumentType
 
 
 class ModuleInfo(BaseModuleInfo):
     module_type_name = "vocab_counter"
     module_readable_name = "Token frequency counter"
-    module_inputs = [("corpus", TarredCorpusType(IntegerListsDocumentType)), ("vocab", Dictionary)]
-    module_outputs = [("distribution", NumpyArray)]
+    module_inputs = [("corpus", GroupedCorpus(IntegerListsDocumentType())), ("vocab", Dictionary())]
+    module_outputs = [("distribution", NumpyArray())]
     module_options = {
         "oov_excluded": {
             "help": "Indicates that the corpus has been mapped so that OOVs are represented "
@@ -45,4 +41,3 @@ class ModuleInfo(BaseModuleInfo):
 
     def get_software_dependencies(self):
         return [numpy_dependency] + super(ModuleInfo, self).get_software_dependencies()
-
