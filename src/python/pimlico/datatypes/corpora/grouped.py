690015d34e0775e4e5b21df4538503bb5b48257a
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-01-10 15:22:44 +0200

    Fixed import problem in core library
    Addition of json datatypes module meant that absolute imports is needed to import the standard json module.

--- src/python/pimlico/datatypes/corpora/grouped.py
+++ src/python/pimlico/datatypes/corpora/grouped.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 # This file is part of Pimlico
 # Copyright (C) 2016 Mark Granroth-Wilding
 # Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
