169afe1385757873c23cdfa3e422096499267ba3
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-06-20 10:19:55 +0300

    Added Py2-only core dependencies

--- src/python/pimlico/core/dependencies/core.py
+++ src/python/pimlico/core/dependencies/core.py
@@ -1,8 +1,10 @@
 # This file is part of Pimlico
 # Copyright (C) 2016 Mark Granroth-Wilding
 # Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
-
 """ Basic Pimlico core dependencies """
+
+from future.utils import PY2
+
 from pimlico.core.dependencies.python import PythonPackageOnPip, PythonPackageSystemwideInstall
 
 #: Core dependencies required by the basic Pimlico installation, regardless of what pipeline is being processed.
@@ -25,3 +27,10 @@ CORE_PIMLICO_DEPENDENCIES = [
     # We use the future library to provide Python 2-3 compatibility
     PythonPackageOnPip("future"),
 ]
+
+# Python 2-only dependencies
+if PY2:
+    CORE_PIMLICO_DEPENDENCIES.extend([
+        # Provides a backport of Py3's updated and renamed configparser package
+        PythonPackageOnPip("configparser"),
+    ])
