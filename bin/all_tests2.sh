7306739d3b7b85cadddd5975bc2fd30214ae0f54
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-07-14 12:37:09 +0300

    Moved test scripts to subdir
    There are so many now, after adding python 2/3 versions, that we need to hide them away

--- bin/all_tests2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Python 2 tests
-DIR="$(cd "$( dirname $( readlink -f "${BASH_SOURCE[0]}" ))" && pwd )"
-PYTHON_CMD=python2 VIRTUALENV=$DIR/../lib/test_env2 $DIR/python -m unittest discover $DIR/../src/test/python/ "*.py"
