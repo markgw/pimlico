ae464f397b2797dcb754b0b45da644136adc0547
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-09-18 12:22:52 +0100

    Visual status: first steps

--- src/python/pimlico/core/visualize/deps.py
+++ src/python/pimlico/core/visualize/deps.py
@@ -1,24 +1,32 @@
 # This file is part of Pimlico
 # Copyright (C) 2016 Mark Granroth-Wilding
 # Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+"""
+Optional dependency on pydot for visualization.
 
-from pimlico.core.dependencies.base import SystemCommandDependency
-from pimlico.core.dependencies.python import PythonPackageOnPip
-
-
-class GraphvizDependency(SystemCommandDependency):
-    def __init__(self, **kwargs):
-        super(GraphvizDependency, self).__init__("Graphviz", "dot -V", **kwargs)
-
-    def installation_instructions(self):
-        return """\
-Install Graphviz on your system using a package manager, or by following the instructions to
-compile from source at:
-  http://www.graphviz.org/Download.php
-
-For example, on Ubuntu you can run:
-  sudo apt-get install graphviz
+Optional use of pydot based on Keras.
 
 """
 
-pygraphviz_dependency = PythonPackageOnPip("pygraphviz", name="PyGraphviz", dependencies=[GraphvizDependency()])
+try:
+    import pydot
+except ImportError:
+    pydot = None
+
+
+def check_pydot():
+    """Raise errors if `pydot` or GraphViz unavailable."""
+    if pydot is None:
+        raise ImportError(
+            'Failed to import `pydot`. '
+            'Please install `pydot`. '
+            'For example with `pip install pydot`.')
+    try:
+        # Attempt to create an image of a blank graph
+        # to check the pydot/graphviz installation.
+        pydot.Dot.create(pydot.Dot())
+    except OSError:
+        raise OSError(
+            '`pydot` failed to call GraphViz.'
+            'Please install GraphViz (https://www.graphviz.org/) '
+            'and ensure that its executables are in the $PATH.')
--- src/python/pimlico/core/visualize/status.py
+++ src/python/pimlico/core/visualize/status.py
@@ -1,10 +1,12 @@
 # This file is part of Pimlico
 # Copyright (C) 2016 Mark Granroth-Wilding
 # Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
-
-import pygraphviz as pgv
+from pimlico.core.visualize.deps import check_pydot
 
 
 def build_graph_with_status(pipeline, all=False):
-    graph = pgv.AGraph(directed=True)
+    check_pydot()
+    import pydot
+
+
     return None
