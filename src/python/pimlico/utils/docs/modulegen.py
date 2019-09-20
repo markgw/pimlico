74891b743bb655008fadc053377f352169c8fec2
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-09-20 17:42:26 +0100

    Fixed modulegen for newly added output groups

--- src/python/pimlico/utils/docs/modulegen.py
+++ src/python/pimlico/utils/docs/modulegen.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 
 import argparse
 import codecs
+import inspect
 import os
 import sys
 import warnings
@@ -157,6 +158,8 @@ def generate_docs_for_pimlico_mod(module_path, output_dir, submodules=[], test_r
     if further_outputs_doc is None or further_outputs_doc == provide_further_outputs_base_doc:
         # Docstring hasn't been overridden: don't use this
         further_outputs_doc = ""
+    else:
+        further_outputs_doc = inspect.cleandoc(further_outputs_doc)
     # Output groups are simply defined as string
     output_groups_table = [
         [group_name, ", ".join(output_names)]
@@ -165,8 +168,14 @@ def generate_docs_for_pimlico_mod(module_path, output_dir, submodules=[], test_r
     build_output_groups_doc = ModuleInfo.build_output_groups.__doc__
     if build_output_groups_doc is None or build_output_groups_doc == build_output_groups_base_doc:
         build_output_groups_doc = ""
+    else:
+        build_output_groups_doc = inspect.cleandoc(build_output_groups_doc)
     info_doc = info.__doc__
+    if info_doc is not None:
+        info_doc = inspect.cleandoc(info_doc)
     module_info_doc = ModuleInfo.__doc__
+    if module_info_doc is not None:
+        module_info_doc = inspect.cleandoc(module_info_doc)
 
     additional_paras = []
     if ModuleInfo.is_input():
@@ -238,7 +247,7 @@ def generate_docs_for_pimlico_mod(module_path, output_dir, submodules=[], test_r
 
         # Show output groups if there are any
         if len(output_groups_table) or len(build_output_groups_doc):
-            output_file.write(format_heading(1, "Output groups"))
+            output_file.write("\n" + format_heading(1, "Output groups"))
             output_file.write("The module defines some named output groups, which can be used to refer to collections "
                               "of outputs at once, as multiple inputs to another module or alternative inputs.\n")
             if len(output_groups_table):
@@ -248,7 +257,7 @@ def generate_docs_for_pimlico_mod(module_path, output_dir, submodules=[], test_r
 
         # Table of options
         if options_table:
-            output_file.write(format_heading(1, "Options"))
+            output_file.write("\n" + format_heading(1, "Options"))
             output_file.write("%s\n" % make_table(options_table, header=["Name", "Description", "Type"]))
 
         # Example config
