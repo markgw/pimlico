49af96b6cadd59ccf81c2b58b50f1c264419cc04
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-02-10 13:06:54 +0200

    Fixed bug in LDA doc-topic mapping
    Use correct attribute of document object.
    
    Skip invalid docs

--- src/python/pimlico/modules/gensim/lda_doc_topics/execute.py
+++ src/python/pimlico/modules/gensim/lda_doc_topics/execute.py
@@ -1,6 +1,7 @@
 from builtins import range
 from collections import Counter
 
+from pimlico.core.modules.map import skip_invalid
 from pimlico.core.modules.map.multiproc import multiprocessing_executor_factory
 
 
@@ -8,9 +9,10 @@ def worker_set_up(worker):
     worker.model = worker.info.get_input("model").load_model()
 
 
+@skip_invalid
 def process_document(worker, archive_name, doc_name, doc):
     # Get a bag of words for the document
-    bow = list(Counter(word for sentence in doc for word in sentence).items())
+    bow = list(Counter(word for sentence in doc.lists for word in sentence).items())
     # Use the LDA model to infer a topic vector for the document
     topic_weights = dict(worker.model[bow])
     # The weights are a sparse vector: fill in the relevant values and leave the rest as 0
