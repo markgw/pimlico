a3d779a59164dd3f1c1ba635d005af705ebf025f
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-02-10 13:46:50 +0200

    Modified doc-topic mapper to use new output type.
    More efficient and avoids pickling problems with sending doc datatypes between processes.

--- src/python/pimlico/modules/gensim/lda_doc_topics/execute.py
+++ src/python/pimlico/modules/gensim/lda_doc_topics/execute.py
@@ -16,7 +16,7 @@ def process_document(worker, archive_name, doc_name, doc):
     # Use the LDA model to infer a topic vector for the document
     topic_weights = dict(worker.model[bow])
     # The weights are a sparse vector: fill in the relevant values and leave the rest as 0
-    return worker.info.document(vector=[topic_weights.get(i, 0.) for i in range(worker.model.num_topics)])
+    return {"vector": [topic_weights.get(i, 0.) for i in range(worker.model.num_topics)]}
 
 
 ModuleExecutor = multiprocessing_executor_factory(process_document, worker_set_up_fn=worker_set_up)
