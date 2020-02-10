6920956fac5caed9ef8e94add2bc1a855428876e
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-02-10 13:01:11 +0200

    Another attempt to fix vector doc type problem

--- src/python/pimlico/datatypes/corpora/floats.py
+++ src/python/pimlico/datatypes/corpora/floats.py
@@ -195,14 +195,14 @@ class VectorDocumentType(RawDocumentType):
     def reader_init(self, reader):
         super(VectorDocumentType, self).reader_init(reader)
         # Prepare struct for reading in the whole vector
-        dim = reader.datatype.metadata["dimensions"]
+        dim = reader.metadata["dimensions"]
         self.struct = struct.Struct("<" + "d"*dim)
         self.value_size = self.struct.size
 
     def writer_init(self, writer):
         super(VectorDocumentType, self).writer_init(writer)
         # Prepare struct for writing out the whole vector
-        dim = writer.datatype.metadata["dimensions"]
+        dim = writer.metadata["dimensions"]
         self.struct = struct.Struct("<" + "d"*dim)
         self.value_size = self.struct.size
 
