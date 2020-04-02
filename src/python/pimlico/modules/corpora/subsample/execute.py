ed0ed4e589066d611b5d073192453c8c29859b43
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-04-02 13:03:30 +0300

    Added new module to randomly subsample a corpus

---
+++ src/python/pimlico/modules/corpora/subsample/execute.py
@@ -0,0 +1,31 @@
+import random
+
+from pimlico.core.modules.base import BaseModuleExecutor
+from pimlico.datatypes.corpora import is_invalid_doc
+from pimlico.utils.progress import get_progress_bar
+
+
+class ModuleExecutor(BaseModuleExecutor):
+    def execute(self):
+        input_corpus = self.info.get_input("corpus")
+        skip_invalid = self.info.options["skip_invalid"]
+        prob = self.info.options["p"]
+
+        rng = random.Random(self.info.options["seed"])
+        selected = 0
+        total = 0
+
+        with self.info.get_output_writer("corpus") as writer:
+            self.log.info("Randomly sampling docs with a probability of {:.2f}% from corpus of {:,} docs"
+                          .format(prob*100., len(input_corpus)))
+            pbar = get_progress_bar(len(input_corpus), title="Sampling")
+            for archive_name, doc_name, doc in pbar(input_corpus.archive_iter()):
+                # If skipping invalid, check this now
+                if not skip_invalid or not is_invalid_doc(doc):
+                    total += 1
+                    if rng.random() < prob:
+                        # Include this document
+                        writer.add_document(archive_name, doc_name, doc)
+                        selected += 1
+
+        self.log.info("Included {:,}/{:,} docs in output corpus".format(selected, total))
---
+++ src/python/pimlico/modules/corpora/subsample/info.py
@@ -0,0 +1,38 @@
+# This file is part of Pimlico
+# Copyright (C) 2016 Mark Granroth-Wilding
+# Licensed under the GNU GPL v3.0 - http://www.gnu.org/licenses/gpl-3.0.en.html
+
+"""
+Randomly subsample documents of a corpus at a given rate to create a smaller corpus.
+
+"""
+from pimlico.core.modules.base import BaseModuleInfo
+from pimlico.core.modules.options import str_to_bool
+from pimlico.datatypes import GroupedCorpus
+from pimlico.datatypes.corpora.grouped import CorpusWithTypeFromInput
+
+
+class ModuleInfo(BaseModuleInfo):
+    module_type_name = "subsample"
+    module_readable_name = "Random subsample"
+    module_inputs = [("corpus", GroupedCorpus())]
+    module_outputs = [("corpus", CorpusWithTypeFromInput())]
+    module_options = {
+        "p": {
+            "help": "Probability of including any given document. The resulting corpus will be roughly "
+                    "this proportion of the size of the input",
+            "type": float,
+            "required": True,
+            "example": "0.1",
+        },
+        "skip_invalid": {
+            "help": "Skip over any invalid documents so that the output subset contains just valid document "
+                    "and no invalid ones. By default, invalid documents are passed through",
+            "type": str_to_bool,
+        },
+        "seed": {
+            "help": "Random seed. We always set a random seed before starting to ensure some level of reproducability",
+            "type": int,
+            "default": 1234,
+        },
+    }
