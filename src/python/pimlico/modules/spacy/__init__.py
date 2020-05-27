ed7a8762ff0bce2434b061b29485ba4f6ca285cb
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-05-27 15:48:53 +0300

    Added spacy tokenizer

---
+++ src/python/pimlico/modules/spacy/__init__.py
@@ -0,0 +1,10 @@
+"""
+Run spaCy tools and pipelines on your datasets.
+
+Currently only includes tokenization, but this could be expanded to include
+many more of spaCy's tools.
+
+Or, if you want a different tool/pipeline, you could create your own
+module type following the same approach.
+
+"""
---
+++ src/python/pimlico/modules/spacy/tokenize/execute.py
@@ -0,0 +1,57 @@
+from importlib import reload
+
+import spacy
+from spacy import about
+from spacy.cli.download import get_json, get_compatibility, get_version, download_model
+
+from pimlico.core.modules.execute import ModuleExecutionError
+from pimlico.core.modules.map import skip_invalid
+from pimlico.core.modules.map.singleproc import single_process_executor_factory
+
+
+def preprocess(executor):
+    model = executor.info.options["model"]
+    if not executor.info.options["on_disk"]:
+        # If not loading from disk, we need to run the spacy download command
+        executor.log.info("Checking the model '{}' is downloaded".format(model))
+        if not download(model):
+            raise ModuleExecutionError("Model could not be downloaded")
+
+    nlp = spacy.load(model)
+    executor.tokenizer = nlp.Defaults.create_tokenizer(nlp)
+    executor.sentencizer = nlp.create_pipe("sentencizer")
+
+
+@skip_invalid
+def process_document(worker, archive, filename, doc):
+    # Apply tokenization to the raw text
+    doc = worker.executor.tokenizer(doc.text)
+    # Apply sentence segmentation to the doc
+    doc = worker.executor.sentencizer(doc)
+
+    # Now doc.sents contains the separated sentences
+    return {"sentences": [[token.text for token in sent] for sent in doc.sents]}
+
+
+ModuleExecutor = single_process_executor_factory(process_document, preprocess_fn=preprocess)
+
+
+def download(model):
+    """
+    Replicates what spaCy does in its cmdline interface.
+
+    """
+    dl_tpl = "{m}-{v}/{m}-{v}.tar.gz#egg={m}=={v}"
+    shortcuts = get_json(about.__shortcuts__, "available shortcuts")
+    model_name = shortcuts.get(model, model)
+    compatibility = get_compatibility()
+    version = get_version(model_name, compatibility)
+    dl = download_model(dl_tpl.format(m=model_name, v=version))
+    # Returns 0 if download was successful
+    if dl != 0:
+        return False
+
+    # Refresh sys.path so we can import the installed package
+    import site
+    reload(site)
+    return True
