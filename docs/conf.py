83fb734bf703220c05da28c532c79af68924114b
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-08-03 14:45:30 +0300

    Small doc config change

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,10 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    # Don't show next/previous buttons, as they're not very useful
+    "prev_next_buttons_location": None,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
